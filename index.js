56032df3995b801e16ee9b3fca708e4b031a2d63
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-18 09:39:51 -0500

    creando los endpoints usando una ruta con parametros

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const port = process.env.PORT || 9000;
 app.use(bodyParser.urlencoded({extended: false}) );
 app.use(bodyParser.json());
 
+//endpoints
+app.get('/home/:name', (req,res)=>{
+    res.send({
+        message: `Welcome ${req.params.name} a mi api REST `
+    })
+});
+
 // server connect
 const server = app.listen(port,()=>{
     console.log(`Run server http://localhost:${ server.address().port }`);
