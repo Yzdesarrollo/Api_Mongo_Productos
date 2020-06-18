4b4e7791679ba13ff188c5f639dcfe6833dc2b1e
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-18 08:58:01 -0500

    creando los middlewares con body-parser

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+// requires
 const express = require('express');
 const bodyParser = require('body-parser');
+// server
 const app = express();
 const port = process.env.PORT || 9000;
 
+//middlewares
+app.use(bodyParser.urlencoded({extended: false}) );
+app.use(bodyParser.json());
+
+// server connect
 const server = app.listen(port,()=>{
     console.log(`Run server http://localhost:${ server.address().port }`);
 });
