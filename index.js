1439e6e6d8655b9a0eb3b47a8063163d615ea2dc
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-18 08:50:33 -0500

    instalando nodemon y body-parser

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 9000;
 
 const server = app.listen(port,()=>{
     console.log(`Run server http://localhost:${ server.address().port }`);
 });
+
+
 
