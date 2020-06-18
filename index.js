dc82cfe5fac2050d49e72a741796d735930b3d66
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-18 11:21:59 -0500

    Usando mongoose y conectando la api a BD

--- index.js
+++ index.js
@@ -7,37 +7,44 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 //middlewares
-app.use(bodyParser.urlencoded({extended: false}) );
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //endpoints
 // Get en general
-app.get('/api/product', (req, res)=>{
-   res.send(200, {products: []})
+app.get('/api/product', (req, res) => {
+    res.send(200, { products: [] })
 });
 
 // Get individual
-app.get('/api/product/:productId', (req, res)=> {
+app.get('/api/product/:productId', (req, res) => {
 
 });
 
-app.post('/api/product', (req, res) =>{
+app.post('/api/product', (req, res) => {
     console.log(req.body);
-    res.status(200).send( {message: 'Product OK'} )
+    res.status(200).send({ message: 'Product OK' })
 });
 
-app.put('/api/product/:productId', ()=>{
+app.put('/api/product/:productId', () => {
 
 });
 
-app.delete('/api/product/:productId', (req, res)=>{
+app.delete('/api/product/:productId', (req, res) => {
 
 });
 
 // server connect
-const server = app.listen(port,()=>{
-    console.log(`Run server http://localhost:${ server.address().port }`);
-});
+mongoose.connect('mongodb://localhost:27017/productsyzm', (error, res) => {
+    if(error){
+        return console.log(`Error al conectar a la base de datos: ${error}`)
+    }
+    console.log('Conexion establecida a la BD');
 
+    const server = app.listen(port,()=>{
+        console.log(`API REST corriendo en: http://localhost:${ server.address().port }`)
+    });
+    
+});
 
 
