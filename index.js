610992c967a881930c8ae4e306dade2632b938c4
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-18 13:31:55 -0500

    usando el modelo y relizando peticiones con el GET general y GET individual

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 
 // import Schema o models
 const ProductModel = require('./models/productModel');
+const e = require('express');
 
 // server
 const app = express();
@@ -17,22 +18,36 @@ app.use(bodyParser.json());
 //endpoints
 // Get en general
 app.get('/api/product', (req, res) => {
-    res.send(200, { products: [] })
+
+    ProductModel.find({}, (err, products) => {
+        if (err) return res.status(500).send({ message: `Error al realizar la peticion ${err}` })
+        if (!products) return res.status(404).send({ message: 'No existen los productos' })
+
+        res.status(200).send({ products })
+    })
 });
 
 // Get individual
 app.get('/api/product/:productId', (req, res) => {
+    let productId = req.params.productId
+
+    ProductModel.findById(productId, (err, product) => {
+        if (err) return res.status(500).send({ message: `Error al realizar la peticion ${err}` })
+        if (!product) return res.status(404).send({ message: `El producto no existe` })
+
+        res.status(200).send({ product: product })
+    })
 
 });
 
 app.post('/api/product', (req, res) => {
-    // instanciando el modelo
-    let products = new ProductModel()
 
     //probando si llegan los datos
     console.log('POST:/api/product');
     console.log(req.body);
-    
+
+    // instanciando el modelo
+    let products = new ProductModel()
 
     // propiedades del modelo
     products.name = req.body.name
@@ -40,9 +55,9 @@ app.post('/api/product', (req, res) => {
     products.category = req.body.category
     products.image = req.body.image
 
-    products.save( (err, data) =>{
-        if(err) res.status(500).send( {message: `no se pudo guardar los datos: ${err}`} )
-        res.status(200).send( {products: data} )
+    products.save((err, data) => {
+        if (err) return res.status(500).send({ message: `no se pudo guardar los datos: ${err}` })
+        res.status(200).send({ products: data })
     })
 });
 
@@ -56,15 +71,15 @@ app.delete('/api/product/:productId', (req, res) => {
 
 // server connect
 mongoose.connect('mongodb://localhost:27017/productsyzm', (error, res) => {
-    if(error){
+    if (error) {
         return console.log(`Error al conectar a la base de datos: ${error}`)
     }
     console.log('Conexion establecida a la BD');
 
-    const server = app.listen(port,()=>{
-        console.log(`API REST corriendo en: http://localhost:${ server.address().port }`)
+    const server = app.listen(port, () => {
+        console.log(`API REST corriendo en: http://localhost:${server.address().port}`)
     });
-    
+
 });
 
 
